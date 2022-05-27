d51524eccafadbb1ae65c6fa65b1c2a2eb281b3c
nanofrks <softivyad@gmail.com>
2022-05-27 16:57:01 -0500

    conf

---
+++ app/Dockerfile
@@ -0,0 +1,9 @@
+FROM ubuntu
+RUN apt update
+RUN apt install python3 -y
+RUN apt install python3-pip -y
+RUN pip3 install dash
+RUN pip3 install pandas
+RUN pip3 install openpyxl
+COPY app.py /
+EXPOSE 80
---
+++ app/app.py
@@ -0,0 +1,19 @@
+from dash import Dash, html, dcc
+import pandas as pd
+import plotly.graph_objs as go
+
+app = Dash(__name__)
+
+def server_layout():
+    df = pd.read_excel('volumen/datos.xlsx')
+    return html.Div([html.H1('piedras preciosas'),
+                        html.Div('amo las piedras'),
+                        html.Div(df['Articulo 1']),
+                        html.Div(df['Articulo 2']),
+                        html.Div(df['Articulo 3']),
+                        html.Dib(df['Articulo 4']),])
+
+app.layout = server_layout
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0',port=80)
